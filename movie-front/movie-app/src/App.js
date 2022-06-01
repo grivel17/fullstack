c4550480991f1325485ff73ca0ea25a48d05b8d3
Tomasz Wojciechowski <tomasz.wojciechowski@sollers.eu>
2022-06-01 10:07:40 +0200

    first

--- movie-front/movie-app/src/App.js
+++ movie-front/movie-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Inputs from "./Inputs";
+import RefTest from "./RefTest";
 
 function App() {
   return (
     <div className="App">
       <p>Appka Tomka</p>
       <Inputs />
+      <RefTest />
     </div>
   );
 }
